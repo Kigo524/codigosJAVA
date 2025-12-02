7694a153a4aada1a09762a83d1caab4ed24225c4
Kigo524 <kigocode@gmail.com>
2025-12-01 20:56:12 -0600

    Agrego un buscador de letra para que pueda agregar una nueva letra si la palabra nueva es con letra nueva

--- Diccionario/Diccionario.java
+++ Diccionario/Diccionario.java
@@ -12,5 +12,24 @@ public class Diccionario {
         this.letras = new ArrayList<>();
     }
 
-    
+    public void agregarPalabra(String palabra, String definicion){
+        //toLowerCase es para dejar todo en minusculas
+        String termino = palabra.toLowerCase(); //cambio a termino porqeu sino se duplica definicion
+        if(termino.isEmpty()) return; //agrega una palabra pero crea una Letra si es necesario
+
+        char inicial = termino.charAt(0); //si no existe esa palabra...
+        Palabra nuevaPalabra = new Palabra(termino, definicion);
+
+        
+    }
+
+    //aqui quiero que busque la letra para que la cree si es una palabra nueva con letra nueva
+    private Letra buscarLetra(char inicial){
+        for(Letra l : letras){
+            if(l.getLetra() == inicial){
+                return l;
+            }
+        }
+        return null;
+    }
 }
