b113c204d323192ae285990757d430106cfaa392
Kigo524 <kigocode@gmail.com>
2025-12-12 00:55:07 -0600

    agrego la clase hija "Libro"

---
+++ ProyectoF_Biblioteca/Libro.java
@@ -0,0 +1,16 @@
+package ProyectoF_Biblioteca;
+
+public class Libro extends MaterialBibliografico{
+    //atributos
+    private String editorial;
+    private int numPaginas;
+    private int numEdicion;
+
+    //constuctor
+    public Libro(String id, String titulo, String autor, int anioPublicacion, String editorial, int numPaginas, int numEdicion){
+        super(id, titulo, autor, anioPublicacion);
+        this.editorial=editorial;
+        this.numPaginas=numPaginas;
+        this.numEdicion=numEdicion;
+    }
+}
