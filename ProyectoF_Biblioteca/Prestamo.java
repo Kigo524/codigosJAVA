90e3e1574754c30c3267148db1a1ce4e751969ad
Kigo524 <kigocode@gmail.com>
2025-12-16 17:56:59 -0600

    hago el constructor y los getter y setters de Prestamo, pero aun me falta corregir algunos tipos de getters

--- ProyectoF_Biblioteca/Prestamo.java
+++ ProyectoF_Biblioteca/Prestamo.java
@@ -1,6 +1,31 @@
 package ProyectoF_Biblioteca;
 
+import java.time.LocalDate;
+
 public class Prestamo {
     //atributos
-    
+    private String idPrestamo;
+    private MaterialBibliografico material;
+    private String matriculaUsuario;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucion;
+
+    public Prestamo(String idPrestamo, MaterialBibliografico material, String matriculaUsuario){
+        this.idPrestamo=idPrestamo;
+        this.material= material;
+        this.matriculaUsuario=matriculaUsuario;
+        this.fechaPrestamo=LocalDate.now(); //para la fecha de hoy
+        this.fechaDevolucion=null; //inicia en null por si es fecha indeterminada
+    }
+
+    //getters y setters
+    public String getIdPrestamo(){return idPrestamo;} //creo que no ncesito set de esto
+
+    public MaterialBibliografico getMaterial(){return material;}
+
+    public String getMatriculaUsuario(){return matriculaUsuario;}
+
+    public LocalDate getFechaPrestamo(){return fechaPrestamo;}
+
+    public LocalDate getFechaDevolucion(){return fechaDevolucion;}
 }
