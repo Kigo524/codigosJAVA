a4ece1546eb3968aa93715139b8e63ae7dfb32b6
Kigo524 <kigocode@gmail.com>
2025-12-16 18:03:18 -0600

    Agrego un creador de IDs unico y hago las sustituciones necesarias, tambien agrego el set de fechaDevolucion

--- ProyectoF_Biblioteca/Prestamo.java
+++ ProyectoF_Biblioteca/Prestamo.java
@@ -1,17 +1,19 @@
 package ProyectoF_Biblioteca;
 
 import java.time.LocalDate;
+import java.util.UUID; //para generar IDs unicos
 
 public class Prestamo {
     //atributos
     private String idPrestamo;
     private MaterialBibliografico material;
-    private String matriculaUsuario;
+    private String matriculaUsuario; //desde Usuario
     private LocalDate fechaPrestamo;
     private LocalDate fechaDevolucion;
 
-    public Prestamo(String idPrestamo, MaterialBibliografico material, String matriculaUsuario){
-        this.idPrestamo=idPrestamo;
+    //constructor
+    public Prestamo(MaterialBibliografico material, String matriculaUsuario){
+        this.idPrestamo=UUID.randomUUID().toString(); //esto es para que genere un id random en lugar de un contador
         this.material= material;
         this.matriculaUsuario=matriculaUsuario;
         this.fechaPrestamo=LocalDate.now(); //para la fecha de hoy
@@ -28,4 +30,9 @@ public class Prestamo {
     public LocalDate getFechaPrestamo(){return fechaPrestamo;}
 
     public LocalDate getFechaDevolucion(){return fechaDevolucion;}
+    public void setFechaDevolucion(LocalDate fechaDevolucion){
+        this.fechaDevolucion = fechaDevolucion; //ahora si por si hay que registrar fecha de devuelto
+    }
+
+    
 }
